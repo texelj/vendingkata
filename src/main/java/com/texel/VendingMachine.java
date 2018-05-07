8cf09fcd71114e8fbf27a40d954406a0c35a714e
Jacob <jacobtexel@gmail.com>
2018-05-06 22:57:20 -0400

    Replaced values with constants or references to enum.

--- src/main/java/com/texel/VendingMachine.java
+++ src/main/java/com/texel/VendingMachine.java
@@ -7,6 +7,14 @@ import java.util.List;
  * Created by jacob on 5/4/2018.
  */
 public class VendingMachine {
+    private static final int NICKEL_VALUE = 5;
+    private static final int DIME_VALUE = 10;
+    private static final int QUARTER_VALUE = 25;
+    private static final int DEFAULT_STOCK_COUNT = 10;
+    private static final String SOLD_OUT_MESSAGE = "SOLD OUT";
+    private static final String COMPLETE_PURCHASE_MESSAGE = "THANK YOU";
+    private static final String EXACT_CHANGE_MESSAGE = "EXACT CHANGE ONLY";
+    private static final String DEFAULT_DISPLAY_MESSAGE = "INSERT COIN";
     private int totalInserted;
     private boolean productSelected;
     private String productDisplay;
@@ -14,9 +22,7 @@ public class VendingMachine {
     private CoinStock coinStock;
     private CoinStock coinsInserted;
 
-    private static final int DEFAULT_STOCK_COUNT = 10;
-
-    public VendingMachine(){
+    public VendingMachine() {
         this(new ProductStock(DEFAULT_STOCK_COUNT), new CoinStock(DEFAULT_STOCK_COUNT));
     }
 
@@ -25,46 +31,46 @@ public class VendingMachine {
     }
 
     public VendingMachine(CoinStock initialStock) {
-        this(new ProductStock(DEFAULT_STOCK_COUNT),initialStock);
+        this(new ProductStock(DEFAULT_STOCK_COUNT), initialStock);
     }
 
-    public VendingMachine(ProductStock initialProducts, CoinStock initialCoins){
-        totalInserted=0;
-        productSelected=false;
-        productDisplay="";
+    public VendingMachine(ProductStock initialProducts, CoinStock initialCoins) {
+        totalInserted = 0;
+        productSelected = false;
+        productDisplay = "";
         this.productStock = initialProducts;
         this.coinStock = initialCoins;
         coinsInserted = new CoinStock(0);
     }
 
-    public String readDisplay(){
-        if(productSelected){
+    public String readDisplay() {
+        if (productSelected) {
             productSelected = false;
             return productDisplay;
         }
-        if(this.totalInserted>0){
-            return String.format("%.2f",(float)totalInserted/100);
+        if (this.totalInserted > 0) {
+            return String.format("%.2f", (float) totalInserted / 100);
         }
-        if(exactChangeNeeded()){
-            return "EXACT CHANGE ONLY";
+        if (exactChangeNeeded()) {
+            return EXACT_CHANGE_MESSAGE;
         }
-        return "INSERT COIN";
+        return DEFAULT_DISPLAY_MESSAGE;
     }
 
     private boolean exactChangeNeeded() {
         //Cannot make 5 cent change
-        if(coinStock.getCount(Coin.NICKEL)<=0)
+        if (coinStock.getCount(Coin.NICKEL) <= 0)
             return true;
         //Cannot make 10 cent change
-        if(coinStock.getCount(Coin.NICKEL)<2 && coinStock.getCount(Coin.DIME)<=0)
+        if (coinStock.getCount(Coin.NICKEL) < 2 && coinStock.getCount(Coin.DIME) <= 0)
             return true;
         //Cannot make 15 cent change
-        if(coinStock.getCount(Coin.DIME)<=0 && coinStock.getCount(Coin.NICKEL)<3
-                || coinStock.getCount(Coin.DIME)>=1 && coinStock.getCount(Coin.NICKEL)<=0)
+        if (coinStock.getCount(Coin.DIME) <= 0 && coinStock.getCount(Coin.NICKEL) < 3
+                || coinStock.getCount(Coin.DIME) >= 1 && coinStock.getCount(Coin.NICKEL) <= 0)
             return true;
         //Cannot make 20 cent change
-        if(coinStock.getCount(Coin.DIME)<=0 && coinStock.getCount(Coin.NICKEL)<4
-                || coinStock.getCount(Coin.DIME)==1 && coinStock.getCount(Coin.NICKEL)<2)
+        if (coinStock.getCount(Coin.DIME) <= 0 && coinStock.getCount(Coin.NICKEL) < 4
+                || coinStock.getCount(Coin.DIME) == 1 && coinStock.getCount(Coin.NICKEL) < 2)
             return true;
         return false;
     }
@@ -72,11 +78,11 @@ public class VendingMachine {
     public Coin[] insertCoin(Coin coin) {
         Coin[] coinReturn = new Coin[]{};
         int coinValue = determineCoinValue(coin);
-        if(coinValue>0){
-            coinsInserted.addStock(coin,1);
-            totalInserted+=coinValue;
+        if (coinValue > 0) {
+            coinsInserted.addStock(coin, 1);
+            totalInserted += coinValue;
         } else {
-            coinReturn = new Coin[] {coin};
+            coinReturn = new Coin[]{coin};
         }
         return coinReturn;
     }
@@ -84,33 +90,33 @@ public class VendingMachine {
     public Coin[] selectProduct(Product product) {
         Coin[] coinReturn = new Coin[]{};
         productSelected = true;
-        if(productStock.getCount(product)<=0){
-            productDisplay = "SOLD OUT";
-        } else if(totalInserted<product.price()){
-            productDisplay=String.format("PRICE %.2f",(float)product.price()/100);
+        if (productStock.getCount(product) <= 0) {
+            productDisplay = SOLD_OUT_MESSAGE;
+        } else if (totalInserted < product.price()) {
+            productDisplay = String.format("PRICE %.2f", (float) product.price() / 100);
         } else {
-            productDisplay="THANK YOU";
+            productDisplay = COMPLETE_PURCHASE_MESSAGE;
             addInsertedCoinToStock();
             coinReturn = makeChange(totalInserted - product.price());
             clearInserted();
-            productStock.removeStock(product,1);
+            productStock.removeStock(product, 1);
         }
         return coinReturn;
     }
 
     private void addInsertedCoinToStock() {
-        for(Coin c: Coin.values()){
-            coinStock.addStock(c,coinsInserted.getCount(c));
+        for (Coin c : Coin.values()) {
+            coinStock.addStock(c, coinsInserted.getCount(c));
         }
     }
 
-    private Coin[] makeChange(int changeTotal){
+    private Coin[] makeChange(int changeTotal) {
         List<Coin> coins = new ArrayList<>();
-        for(Coin c: Coin.values()){
+        for (Coin c : Coin.values()) {
             int coinValue = determineCoinValue(c);
-            if(coinValue>0) {
+            if (coinValue > 0) {
                 int coinChange = Math.min(changeTotal / coinValue, coinStock.getCount(c));
-                if(coinChange>0) {
+                if (coinChange > 0) {
                     changeTotal -= coinChange * coinValue;
                     coinStock.removeStock(c, coinChange);
                     for (; coinChange > 0; coinChange--) coins.add(c);
@@ -121,12 +127,12 @@ public class VendingMachine {
     }
 
     private int determineCoinValue(Coin c) {
-        if(c.diameter()==24.3 && c.mass()==5.6){
-            return 25;
-        } else if(c.mass()==2.3 && c.diameter()==17.9){
-            return 10;
-        } else if(c.mass()==5.0 && c.diameter()==21.2){
-            return 5;
+        if (c.mass() == Coin.QUARTER.mass() && c.diameter() == Coin.QUARTER.diameter()) {
+            return QUARTER_VALUE;
+        } else if (c.mass() == Coin.DIME.mass() && c.diameter() == Coin.DIME.diameter()) {
+            return DIME_VALUE;
+        } else if (c.mass() == Coin.NICKEL.mass() && c.diameter() == Coin.NICKEL.diameter()) {
+            return NICKEL_VALUE;
         } else {
             return 0;
         }
@@ -134,15 +140,15 @@ public class VendingMachine {
 
     public Coin[] returnCoins() {
         List<Coin> returnCoins = new ArrayList<Coin>();
-        for(Coin c: Coin.values()){
-            for(int i=0; i<coinsInserted.getCount(c); i++)
+        for (Coin c : Coin.values()) {
+            for (int i = 0; i < coinsInserted.getCount(c); i++)
                 returnCoins.add(c);
         }
         clearInserted();
         return returnCoins.toArray(new Coin[0]);
     }
 
-    private void clearInserted(){
+    private void clearInserted() {
         totalInserted = 0;
         coinsInserted = new CoinStock(0);
     }
